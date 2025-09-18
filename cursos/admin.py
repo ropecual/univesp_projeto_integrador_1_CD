7ea2cfdd29fc04f8e75af9d00b9350782d011994
Roni Peterson Alvarenga Ferraresso <rope.alvarenga@gmail.com>
2025-09-18 15:37:41 -0300

    Criação inicial das novas funcionalidades

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Curso, Modulo, Conteudo
+
+admin.site.register(Curso)
+admin.site.register(Modulo)
+admin.site.register(Conteudo)
